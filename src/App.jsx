68e9255224a6ac0b66bfb37ef28cd45e00a6ca06
xuanloi1999 <55650190+xuanloi1999@users.noreply.github.com>
2025-10-29 02:39:28 +0700

    setup font and fontawsome

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { faHouse } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function App() {
   return (
     <p className="p-4 pt-2 text-red-700">
       Click on the Vite and React logos to learn more
+      <FontAwesomeIcon icon={faHouse} />
     </p>
   );
 }
